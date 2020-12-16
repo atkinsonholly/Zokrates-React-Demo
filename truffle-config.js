27952e9e345621465d82cde40249463af53753a0
Timothy Coleman <timhc22@gmail.com>
2020-12-16 05:40:50 +0000

    Update truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -27,5 +27,18 @@
 const path = require("path");
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  networks: {
+    // Useful for testing. The `development` name is special - truffle uses it by default
+    // if it's defined here and no other network is specified at the command line.
+    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+    // tab if you use this network and you must also set the `host`, `port` and `network_id`
+    // options below to some value.
+    //
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 7545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
+  },
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
 };
